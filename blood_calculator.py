bffe7314439ef3ea11ff9891f8d1ae8a52983909
Scout Rice <ainsley.rice@duke.edu>
2022-09-05 12:53:14 -0400

    Add a check HDL function to analyze characterization

--- blood_calculator.py
+++ blood_calculator.py
@@ -12,4 +12,13 @@ def input_HDL():
     HDL_input = input("Enter the HDL value: ")
     return int(HDL_input)
 
+def check_HDL(HDL_value):
+    if HDL_value >= 60:
+        return "Normal"
+    elif HDL_value < 40:
+        return "Low"
+    else:
+        return "Borderline Low"
+
 input_HDL()
+check_HDL()
