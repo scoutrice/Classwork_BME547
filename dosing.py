4764417b8fec53e60d20a317877bbccec9c296d7
Scout Rice <ainsley.rice@duke.edu>
2022-09-12 13:03:46 -0400

    breaking up script

--- dosing.py
+++ dosing.py
@@ -6,7 +6,7 @@
 """
 
 
-def dose_amount():
+def dose_type():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -15,6 +15,8 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+
+def weight_calc():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
@@ -24,14 +26,22 @@ def dose_amount():
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+
+def dosage_calc(diagnosis, weight):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+
+def output_dosage():
+    diag = dose_type()
+    weight1 = weight_calc()
+    dosage = dosage_calc(diag, weight1)
     print("CORRECT DOSAGE")
-    print("For a patient weighing {:.1f} kg,".format(weight))
+    print("For a patient weighing {:.1f} kg,".format(weight1))
     print("  the correct dosage is {:.1f} mg the first day"
-          .format(dosage_mg_first_day))
+          .format(dosage))
 
 
 if __name__ == '__main__':
-    dose_amount()
+    output_dosage()
+    
