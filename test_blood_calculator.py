81ce4bb97555b4e561257c1869c8381b4880f9a1
Scout Rice <ainsley.rice@duke.edu>
2022-09-19 13:14:22 -0400

    Updating in class to match PEP-8 standards

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -1,11 +1,14 @@
 import pytest
 
+
 @pytest.mark.parametrize("input_one, expected", [(85, "Normal"), (50, "Borderline Low"), (30, "Low")])
+
 
 def test_check_HDL(input_one, expected):
     from blood_calculator import check_HDL
     answer = check_HDL(input_one)
     assert answer == expected
+
 
 '''
 def test_check_HDL_Normal():
