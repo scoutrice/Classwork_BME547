d40b0c400a896c5d1d344fa74736f6b390d22d2b
Scout Rice <ainsley.rice@duke.edu>
2022-10-24 12:21:07 -0400

    updates on monday to json

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import requests 
 
-r = requests.get("http://127.0.0.1:500/info")
+r = requests.get("http://127.0.0.1:500/add/2/3")
 print(r.status_code)
 print(r.text)
 
--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+import json
+
 
 app = Flask(__name__)
 
@@ -8,11 +11,24 @@ def server_status():
     return "Server is on."
 
 
-@app.route("/info", methods=["GET"])
-def information():
-    x = "This website will calculate blood cholesterol levels.\n"
-    x += "It is written by David Ward."
-    return x
+# @app.route("/info", methods=["GET"])
+# def information():
+#     x = "This website will calculate blood cholesterol levels.\n"
+#     x += "It is written by David Ward."
+#     return x
+
+@app.route("/addnumbers", methods=["POST"])
+def add_numbers():
+    in_data = request.get_json()
+    answer = in_data["a"] + in_data["b"]
+    return jsonify(answer)
+
+
+@app.route("/add/<a>/<b>", methods=["GET"])
+def add_variable_url(a, b):
+    answer = int(a) + int(b)
+    return jsonify(answer)
+
 
 
 if __name__ == "__main__":
